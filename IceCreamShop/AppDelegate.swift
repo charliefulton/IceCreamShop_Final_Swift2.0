f8b308a201e4bbb6258561cba2e1cf3f174e559d
Charlie Fulton <charliefulton@gmail.com>
2016-04-26 15:26:40 -0400

    setup to use private pod

--- IceCreamShop/AppDelegate.swift
+++ IceCreamShop/AppDelegate.swift
@@ -1,17 +1,27 @@
-//
-//  AppDelegate.swift
-//  IceCreamShop
-//
-//  Created by Joshua Greene on 2/8/15.
-//  Copyright (c) 2015 Razeware, LLC. All rights reserved.
-//
-
 import UIKit
+import RWPickFlavor
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
-  // MARK: Instance Variables
-  
   var window: UIWindow?
+  var rootViewController: UIViewController!
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions
+    launchOptions: [NSObject : AnyObject]?) -> Bool {
+    
+    setupRootViewController()
+    
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
+    
+    return true
+  }
+  
+  func setupRootViewController() {
+    let bundle = NSBundle(forClass: PickFlavorViewController.self)
+    let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+    rootViewController = storyboard.instantiateInitialViewController()!
+  }
 }
--- Podfile
+++ Podfile
@@ -1,9 +1,11 @@
 platform :ios, "8.0"
 use_frameworks!
 
+source 'https://github.com/CocoaPods/Specs.git'
+source 'git@github.com:charliefulton/MyPodSpecs.git'
+
 target 'IceCreamShop' do
-  pod 'Alamofire', '2.0.2'
-  pod 'MBProgressHUD', '~> 0.9.0'
+  pod 'RWPickFlavor', :path => '~/Documents/Libraries/RWPickFlavor'
 end
 
 target 'IceCreamShopTests' do
